363e3c79280e8116d113cba84c19d4c911d1647a
Ndipbanyan <agnesjedidiah09@gmail.com>
2022-11-14 15:31:48 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+	return (
+		<div className=' h-screen bg-red w-screen  font-sans'>
+			<section className=' bg-trail px-20 w-full  text-center text-[#252525] text-sm font-light'>
+				Tada! Get started with a free template. Can’t find what you are looking for? Search from the 1000+ available
+				templates
+			</section>
+		</div>
+	)
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		fontFamily: {
+			sans: ['Circular Std', 'sans-serif'],
+			serif: ['Merriweather', 'serif'],
+		},
+		colors: {
+			trail: '#FFF4EA',
+			red: '#f87171',
+		},
+		extend: {},
+	},
+	plugins: [],
+}
